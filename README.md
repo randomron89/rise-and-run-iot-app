ae85dac45a5644aa19fcaf654f40d32c23f6337e
randomron89 <lenard.uy.1804@gmail.com>
2026-02-06 23:49:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rise_and_run_iot_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/rise_and_run_iot_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rise_and_run_iot_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
